8f3fa448f82591cbf0ffc7b55cee7eab706f3e8d
Kshyamasagar <kshyamasagar.mishra09@gmail.com>
2024-12-03 22:54:27 +0530

    Initial Commit

---
+++ src/main/java/com/sagar/firstJobApp/FirstJobAppApplication.java
@@ -0,0 +1,16 @@
+package com.sagar.firstJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJobAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstJobAppApplication.class, args);
+		System.out.println("hello");
+		
+	}
+
+}
---
+++ src/test/java/com/sagar/firstJobApp/FirstJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.firstJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
